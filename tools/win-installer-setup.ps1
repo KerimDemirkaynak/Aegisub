ed56ac13679634571ed710d28f694b13bf8163aa
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2026-06-23 12:15:49 +0300

    Remove Chinese language downloads from setup script
    Removed downloads for Chinese language files from the installer setup script.

--- tools/win-installer-setup.ps1
+++ tools/win-installer-setup.ps1
@@ -97,8 +97,6 @@ if (!(Test-Path innosetup-langs)) {
 	Invoke-WebRequest https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/Indonesian.isl -OutFile innosetup-langs/Indonesian.isl -UseBasicParsing
 	Invoke-WebRequest https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/SerbianCyrillic.isl -OutFile innosetup-langs/SerbianCyrillic.isl -UseBasicParsing
 	Invoke-WebRequest https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/SerbianLatin.isl -OutFile innosetup-langs/SerbianLatin.isl -UseBasicParsing
-	Invoke-WebRequest https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/ChineseSimplified.isl -OutFile innosetup-langs/ChineseSimplified.isl -UseBasicParsing
-	Invoke-WebRequest https://raw.github.com/jrsoftware/issrc/main/Files/Languages/Unofficial/ChineseTraditional.isl -OutFile innosetup-langs/ChineseTraditional.isl -UseBasicParsing
 }
 
 # Aegisub localization
